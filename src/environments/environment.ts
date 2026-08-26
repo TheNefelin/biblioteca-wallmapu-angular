8b94278c4dc6e687d742b00e873e99dae6631b37
Francisco Carmona <flcarmonac@yahoo.com>
2026-08-26 14:35:52 -0400

    update: add google client id

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   //apiUrl: "http://127.0.0.1:8000/api",
   apiUrl: 'https://biblioteca-wallmapu-python.vercel.app/api',
-  googleClientId: '529904203616-e3rl4utugk5ktbeqbv0ou3itj477gsvd.apps.googleusercontent.com',
+  googleClientId: '802048276546-esf6qbedh56flfna63ismqsdhhn8mg4c.apps.googleusercontent.com',
   version: '1.0.263',
 };

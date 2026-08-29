97a97481e43d6671ed4b1804badbaa565caae452
Francisco Carmona <flcarmonac@yahoo.com>
2026-08-29 13:50:35 -0400

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: "http://127.0.0.1:8000/api",
-  //apiUrl: 'https://biblioteca-wallmapu-python.vercel.app/api',
+  //apiUrl: "http://127.0.0.1:8000/api",
+  apiUrl: 'https://biblioteca-wallmapu-python.vercel.app/api',
   googleClientId: '802048276546-2c7a76rig1978ofmlgeh18hmnj5d66qm.apps.googleusercontent.com',
   version: '1.0.263',
 };

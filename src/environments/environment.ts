76cd9255ececc330eb65d2d567d5e41d9f7a4084
Francisco Carmona <flcarmonac@yahoo.com>
2026-08-30 23:34:36 -0400

    chore: restaurar googleClientId en environment.ts local
    - environment.ts recupera el googleClientId real para permitir el
      login con Google en desarrollo local.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   //apiUrl: 'https://biblioteca-wallmapu-python.vercel.app/api',
   apiUrl: "http://127.0.0.1:8000/api",
-  googleClientId: "",
+  googleClientId: "802048276546-2c7a76rig1978ofmlgeh18hmnj5d66qm.apps.googleusercontent.com",
   version: '1.0.264',
 };
